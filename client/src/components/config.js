7ea14a38be35e5353b735fca4c602bf87bc4964a
AneeshVHegde <1da23is004.is@drait.edu.in>
2026-03-03 22:27:56 +0530

    connect frontend to live backend

--- client/src/components/config.js
+++ client/src/components/config.js
@@ -1,6 +1,6 @@
 // config.js
-
-const baseURL = 'http://localhost:5000'; // Local development server
+const baseURL = "https://lost-found-bj7s.onrender.com";
+//const baseURL = 'http://localhost:5000'; // Local development server
 // const baseURL = 'http://13.61.63.5:5000'; // Your EC2 public IP and backend port
 // const baseURL = "https://lostandfoundbackend-y9qs.onrender.com"; // (Uncomment if using Render in the future)
 const config = {
